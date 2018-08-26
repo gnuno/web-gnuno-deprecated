229ca22628cc4c0f2be7ba5825cd9a7d8062937e
Matias Nicolas Noriega <matiasnoriega86@gmail.com>
2018-08-26 02:49:27 -0300

    login provisorio admin

---
+++ admin/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="icon" href="http://www.favicon.cc/logo3d/19880.png">
+    <title>GNUno - Users GNU/Linux de la UNO</title>
+    <meta charset="utf-8">
+    <link type="text/css" rel="stylesheet" href="../css/css.css">
+    <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+</head>
+<body>
+    <form action="" action="POST">
+        <label for="usuario">Usuario: 
+        <input type="text" name="usuario" id="usuario"></label>
+        <label for="password_u">Password: 
+        <input type="password" name="password_u" id="password_u"></label>
+        <input type="submit">
+    </form>
+</body>
+</html>
