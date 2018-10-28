4f23725d1458af49254e531227a59a735b53c4bc
Enoch Root <enochroot@openmailbox.org>
2018-10-28 11:58:27 -0300

    [Fix] Se corrige enlace a Lanux - se apunta a fanpage de Facebook

--- sections/barra_lateral_der.php
+++ sections/barra_lateral_der.php
@@ -3,7 +3,7 @@
             <h3 class="headerboxes">Amigos</h3>
             <div class="imagenesamigos">
                 <a href="http://flisol.info/" target="_blank"><img src="images/flisol.png" alt="Link Flisol"/></a>
-                <a href="http://www.lanux.org.ar/" target="_blank" ><img src="images/lanux.png" alt="Link Lanux"/></a>
+                <a href="https://es-la.facebook.com/Lanux/" target="_blank" ><img src="images/lanux.png" alt="Link Lanux"/></a>
             </div>
         </nav>
     </div>
